69a2e84388fc024670518c2681509951af9d9a99
Brennan <brennan.buitendag@iotnxt.com>
2021-09-08 19:18:17 +0200

    - chore: More setup

--- apps/responsive-grid-app/src/app/app.component.ts
+++ apps/responsive-grid-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'responsive-grid-root',
+  selector: 'bcb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
---
+++ libs/responsive-grid/README.md
@@ -0,0 +1,7 @@
+# responsive-grid
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test responsive-grid` to execute the unit tests.
